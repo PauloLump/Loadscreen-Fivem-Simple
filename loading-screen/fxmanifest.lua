b90036286cb267a0c978db5e6712da02f80ad623
PauloLump <121923852+PauloLump@users.noreply.github.com>
2023-02-04 09:01:13 -0300

    Add files via upload

---
+++ loading-screen/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "bodacious"
+game "gta5"
+lua54 "yes"
+loadscreen_manual_shutdown "true"
+
+files {
+	"web-side/imgs/*",
+	"web-side/music/*",
+	"web-side/style/*"
+}
+
+loadscreen "web-side/index.html"
